a34c96ceeb78dbacf133cf005a1513bf069a8734
Messi Tom <messi10tombinoy@gmail.com>
2024-10-05 16:40:58 +0530

    ans

--- mathX.py
+++ mathX.py
@@ -3,7 +3,6 @@ from IPython.display import display, Latex
 
 tokenizer = AutoTokenizer.from_pretrained("MESSItom/math-Llama-3.2-1B")
 model = AutoModelForCausalLM.from_pretrained("MESSItom/math-Llama-3.2-1B")
-tokenizer.pad_token = tokenizer.eos_token
 
 alpaca_prompt = """Below is a Maths Question. Write an answer that appropriately completes the request.
 
@@ -13,6 +12,7 @@ alpaca_prompt = """Below is a Maths Question. Write an answer that appropriately
 ### Answer:
 """
 
+# Correct answer is 66
 sample = """Let $A=\{1,2,3, \ldots \ldots \ldots \ldots, 100\}$. Let $R$ be a relation on A defined by $(x, y) \in R$ if and only if $2 x=3 y$. Let $R_1$ be a symmetric relation on $A$ such that $R \subset R_1$ and the number of elements in $R_1$ is n . Then, the minimum value of n is, $\qquad$"""
 text = alpaca_prompt.format(sample)
 display(Latex(text))
