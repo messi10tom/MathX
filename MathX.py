3d26784d8990b6011e249c6b476f5c2c45d3a540
Messi Tom <messi10tombinoy@gmail.com>
2024-10-11 14:54:07 +0530

    update

--- MathX.py
+++ MathX.py
@@ -1,14 +1,6 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from peft import PeftModel 
 
-base_model = "meta-llama/Llama-3.2-1B"
-adapter_model = "MESSItom/mathX-Llama-3.2-1B"
-
-model = AutoModelForCausalLM.from_pretrained(base_model)
-model = PeftModel.from_pretrained(model, adapter_model)
-tokenizer = AutoTokenizer.from_pretrained(adapter_model)
-
-
 
 alpaca_prompt = """Below is a Maths Question. Write an answer that appropriately completes the request.
 
@@ -18,12 +10,23 @@ alpaca_prompt = """Below is a Maths Question. Write an answer that appropriately
 ### Answer:
 """
 
-def generate_answer(sample: str, device,
+def get_model(device):
+    base_model = "meta-llama/Llama-3.2-1B"
+    adapter_model = "MESSItom/mathX-Llama-3.2-1B"
+
+    model = AutoModelForCausalLM.from_pretrained(base_model)
+    model = PeftModel.from_pretrained(model, adapter_model).to(device=device)
+    tokenizer = AutoTokenizer.from_pretrained(adapter_model)
+
+    return model, tokenizer
+
+def generate_answer(model,
+                    tokenizer,
+                    sample: str, 
                     max_new_tokens: int = 500):
-    
-    model = model.to(device=device)
 
     text = alpaca_prompt.format(sample)
+    device = model.device
     model_in = tokenizer(text, return_tensors="pt", truncation=True).to(device)
 
     generation_output = model.generate(**model_in,
--- mathX_sample.py
+++ mathX_sample.py
@@ -1,12 +1,14 @@
 from IPython.display import display, Latex
-from MathX import generate_answer
+from MathX import generate_answer, get_model
+
+model, tokenizer = get_model(device="cuda")
 
 # Correct answer is 66
 sample = """Let $A=\\{1,2,3, \\ldots \\ldots \\ldots \\ldots, 100\\}$. Let $R$ be a relation on A defined by $(x, y) \\in R$ if and only if $2 x=3 y$. Let $R_1$ be a symmetric relation on $A$ such that $R \\subset R_1$ and the number of elements in $R_1$ is n . Then, the minimum value of n is, $\\qquad$"""
-answer = generate_answer(sample, device="cuda")
+answer = generate_answer(model, tokenizer, sample)
 
 print('\n'*5)
 print("Question:\n\n")
-print(display(Latex(sample)))
+display(Latex(sample))
 print("Answer:\n\n")
-print(display(Latex(answer)))
+display(Latex(answer))
